06fb46f5d794b49b7bc36b3e79e05489b76a6100
Mohammad Shoaib <mohammadshoaib@Mohammads-MacBook-Air.local>
2022-08-17 14:57:48 +0600

    new flatten

--- VideoSwinTransformer/PatchEmbed3D.py
+++ VideoSwinTransformer/PatchEmbed3D.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-from tensorflow.keras.layers import  Conv3D
 
 class PatchEmbed3D(tf.keras.Model):
     def __init__(self, patch_size=(2, 4, 4), in_chans=3, embed_dim=96, norm_layer=None):
@@ -10,6 +9,7 @@ class PatchEmbed3D(tf.keras.Model):
         self.in_chans = in_chans
         self.embed_dim = embed_dim
 
+        x = tf.transpose(x, perm=[0, 2,3,4, 1 ])
         
         self.proj = Conv3D(embed_dim, kernel_size=patch_size,
                            strides=patch_size, name='embed_proj')
@@ -29,10 +29,12 @@ class PatchEmbed3D(tf.keras.Model):
         
         if self.norm is not None:
         
-          B, C, D, Wh, Ww = x.get_shape().as_list()
+          B, C, D, Wh, Ww = x.shape
           x = tf.reshape(x, shape=[B, -1, C])
           x = self.norm(x)
+        #   x = tf.reshape(x, shape=[B, C, -1])
           x = tf.reshape(x, shape=[B, C, -1])
+          x = tf.transpose(x, perm=[0 , 2, 1])    
           x = tf.reshape(x, shape=[-1, self.embed_dim, D, Wh, Ww])
 
         return x
